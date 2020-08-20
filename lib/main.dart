2f84fd635b766ebaad8782368156163f91de2322
visprasadh <visprasadhkumaravelu@gmail.com>
2020-08-20 21:22:24 +0530

    Created custom card

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Edwisely extends StatelessWidget {
         fontFamily: 'Poppins',
         primaryColor: Color.fromRGBO(0, 147, 105, 1),
         accentColor: Color.fromRGBO(255, 106, 106, 1),
+        backgroundColor: Color.fromRGBO(229, 229, 229, 1),
       ),
       home: Dashboard(),
     );
--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -5,20 +5,52 @@ import 'package:flutter/material.dart';
 class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Container(
-            height: double.infinity,
-            width: 130,
-            color: Theme.of(context).primaryColor,
-          ),
-          Column(
-            children: [
-              RedChip(label: 'Knowledge'),
-              ListItem(label: 'Lorem ipsum dolor sit amet'),
-            ],
-          ),
-        ],
+      body: Container(
+        color: Theme.of(context).backgroundColor,
+        child: Row(
+          children: [
+            Container(
+              height: double.infinity,
+              width: 130,
+              color: Theme.of(context).primaryColor,
+            ),
+            Column(
+              children: [
+                RedChip(label: 'Knowledge'),
+                ListItem(label: 'Lorem ipsum dolor sit amet'),
+                Container(
+                  width: 904,
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 32),
+                  padding: EdgeInsets.symmetric(vertical: 14, horizontal: 24),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(24),
+                    ),
+                    boxShadow: null,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Course Description',
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromRGBO(116, 116, 116, 1)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
