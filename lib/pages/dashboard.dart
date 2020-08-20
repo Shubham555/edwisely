a0c99b630d819d8ce04c22f99338e7a7f7149406
visprasadh <visprasadhkumaravelu@gmail.com>
2020-08-20 18:20:25 +0530

    Basic

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(
+            height: double.infinity,
+            width: 130,
+            color: Color.fromRGBO(0, 147, 105, 1),
+          ),
+        ],
+      ),
+    );
+  }
+}
