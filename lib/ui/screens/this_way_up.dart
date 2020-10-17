7e743c2e40cdba1c394b8eed04a146959f5c338d
Vishnuprasadh <visprasadhkumaravelu@gmail.com>
2020-10-17 19:03:34 +0530

    Created this_way_up.dart

---
+++ lib/ui/screens/this_way_up.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ThisWayUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.blue,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.screen_rotation,
+                  color: Color(0xFF2A2A2A),
+                ),
+              ),
+              Text(
+                "Please rotate your device to resume",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
